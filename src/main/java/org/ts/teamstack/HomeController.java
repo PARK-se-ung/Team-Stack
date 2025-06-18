33b71016a0f1e49bf1b38cbd7b7c75bdf4b724e4
teacherdev09 <teacherdev09@gmail.com>
2025-06-18 18:15:38 +0900

    bs_test작성

--- src/main/java/org/ts/teamstack/HomeController.java
+++ src/main/java/org/ts/teamstack/HomeController.java
@@ -1,6 +1,7 @@
 package org.ts.teamstack;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,5 +9,10 @@ public class HomeController {
     @RequestMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "test";
     }
 }
