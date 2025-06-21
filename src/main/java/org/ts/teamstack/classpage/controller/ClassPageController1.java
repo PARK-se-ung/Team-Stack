cb9789e7e9ffbe286c61ba87fe9f07d7e60fb5cf
RoastedBear <kinim329@gmail.com>
2025-06-21 16:23:15 +0900

    MI:클래스페이지 사이드바 구현

--- src/main/java/org/ts/teamstack/classpage/controller/ClassPageController1.java
+++ src/main/java/org/ts/teamstack/classpage/controller/ClassPageController1.java
@@ -1,4 +1,18 @@
 package org.ts.teamstack.classpage.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+//@RequiredArgsConstructor
+@RequestMapping("/class")
 public class ClassPageController1 {
+
+//    private final ClassPageService1 classService;
+
+    @RequestMapping("/dashboard")
+    private String dashboard() {
+        return "classes/dashboard";
+    }
+
 }
--- src/main/java/org/ts/teamstack/classpage/model/service/ClassPageServiceImpl1.java
+++ src/main/java/org/ts/teamstack/classpage/model/service/ClassPageServiceImpl1.java
@@ -1,4 +1,8 @@
 package org.ts.teamstack.classpage.model.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ClassPageServiceImpl1 {
+
 }
