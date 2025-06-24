0b45a232ec6951d0d0a7791c35118167c79bad18
sangjin oh <osj4040@gmail.com>
2025-06-24 16:28:23 +0900

    상진 강의개설페이지 수정 및 course mvc 패턴

--- src/main/java/org/ts/teamstack/course/model/dao/CouresDaoImpl.java
+++ src/main/java/org/ts/teamstack/course/model/dao/CouresDaoImpl.java
@@ -3,7 +3,6 @@ package org.ts.teamstack.course.model.dao;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 import org.ts.teamstack.course.model.dto.Course;
-import org.ts.teamstack.user.model.dto.User;
 
 @Repository
 public class CouresDaoImpl implements CousreDao{
