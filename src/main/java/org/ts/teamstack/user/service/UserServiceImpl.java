036c351133c9366936f69859999f7e5be4539788
MyeongSooJang <mmsgod0427@naver.com>
2025-06-25 17:55:21 +0900

    MS 회원가입(수정중) 6/22

--- src/main/java/org/ts/teamstack/user/service/UserServiceImpl.java
+++ src/main/java/org/ts/teamstack/user/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package org.ts.teamstack.user.service;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.ts.teamstack.user.model.dao.UserDaoImpl;
 import org.ts.teamstack.user.model.dto.Users;
@@ -15,6 +16,9 @@ public class UserServiceImpl implements UserService {
     private UserDaoImpl dao;
     @Autowired
     private SqlSession session;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     @Override
     public Users searchById(String id,String password) {
         Users u = dao.searchById(id, session);
@@ -24,7 +28,9 @@ public class UserServiceImpl implements UserService {
         return null;
     }
     @Override
-    public int insertUser(Users user, SqlSession session) {
+    public int insertUser(Users user) {
+        String pwd = passwordEncoder.encode(user.getPassword());
+        user.setUserPwd(pwd);
         return dao.insertUser(user,session);
     }
 }
