65cc2ab3cfc3a914c71a89887714d04a1c868994
MyeongSooJang <mmsgod0427@naver.com>
2025-06-25 09:14:21 +0900

    MS 로그인(중) 6/22

--- src/main/java/org/ts/teamstack/user/model/dto/Users.java
+++ src/main/java/org/ts/teamstack/user/model/dto/Users.java
@@ -4,16 +4,56 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.io.Serializable;
 import java.sql.Date;
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 
-public class Users {
+public class Users implements UserDetails{
     private String userId;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
     private String userPwd;
     private String userName;
     private String userEmail;
--- src/main/java/org/ts/teamstack/user/service/UserService.java
+++ src/main/java/org/ts/teamstack/user/service/UserService.java
@@ -1,5 +1,22 @@
 package org.ts.teamstack.user.service;
 
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.ts.teamstack.user.model.dao.UserDao;
+import org.ts.teamstack.user.model.dto.Users;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserDao dao;
+    private final SqlSession session;
+
+
+    public Users searchById(String userId, SqlSession session){
+        Users u = dao.searchById(userId,session);
+        return u;
+    }
 
 }
