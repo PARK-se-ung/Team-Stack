16f06f686f9269ab48ed37eaa4686b628fcf36dd
MyeongSooJang <mmsgod0427@naver.com>
2025-06-25 09:15:36 +0900

    MS 로그인(중) 6/22

--- src/main/java/org/ts/teamstack/user/service/UserService.java
+++ src/main/java/org/ts/teamstack/user/service/UserService.java
@@ -2,7 +2,6 @@ package org.ts.teamstack.user.service;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.ts.teamstack.user.model.dao.UserDao;
 import org.ts.teamstack.user.model.dto.Users;
@@ -12,7 +11,6 @@ import org.ts.teamstack.user.model.dto.Users;
 public class UserService {
     private final UserDao dao;
     private final SqlSession session;
-
 
     public Users searchById(String userId, SqlSession session){
         Users u = dao.searchById(userId,session);
