0f5ccbf1ce0d28c2bcb63340f5e467043c63d2f7
nunus18 <wosik123@naver.com>
2025-06-25 11:44:48 +0900

    ws 북마크 리스트 조회 06/25

--- src/main/java/org/ts/teamstack/user/model/dao/UserDao.java
+++ src/main/java/org/ts/teamstack/user/model/dao/UserDao.java
@@ -3,12 +3,12 @@ package org.ts.teamstack.user.model.dao;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Repository;
-import org.ts.teamstack.user.model.dto.User;
+import org.ts.teamstack.user.model.dto.Users;
 
 @Repository
 public class UserDao {
 
-    public User searchById (String userId, SqlSession session)
+    public Users searchById (String userId, SqlSession session)
             throws UsernameNotFoundException {
             return session.selectOne("users.searchById",userId);
     }
