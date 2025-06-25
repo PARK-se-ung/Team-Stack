b9149d7f56480d420a242035405573ad69762d06
RoastedBear <kinim329@gmail.com>
2025-06-25 10:45:40 +0900

    MI:클래스페이지 대시보드 페이지 백엔드 구현

--- src/main/java/org/ts/teamstack/classpage/model/dto/Course.java
+++
@@ -1,5 +0,0 @@
-package org.ts.teamstack.classpage.model.dto;
-
-public class Course {
-
-}
--- src/main/java/org/ts/teamstack/classpage/model/service/ClassPageServiceImpl1.java
+++ src/main/java/org/ts/teamstack/classpage/model/service/ClassPageServiceImpl1.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Service;
 import org.ts.teamstack.classpage.model.dao.ClassPageDao1;
-import org.ts.teamstack.classpage.model.dto.Course;
+import org.ts.teamstack.course.model.dto.Course;
 
 import java.util.List;
 
