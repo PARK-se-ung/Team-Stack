b442a6c00b3e2aa89ac747bde1c84447a0a9453c
RoastedBear <kinim329@gmail.com>
2025-06-26 13:51:50 +0900

    MI:클래스메인페이지 사이드바 jsp연결

--- src/main/java/org/ts/teamstack/classpage/controller/ClassPageController1.java
+++ src/main/java/org/ts/teamstack/classpage/controller/ClassPageController1.java
@@ -1,17 +1,11 @@
 package org.ts.teamstack.classpage.controller;
 
 import lombok.RequiredArgsConstructor;
-import oracle.jdbc.proxy.annotation.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.ts.teamstack.classpage.model.dto.Board;
 import org.ts.teamstack.classpage.model.service.ClassPageService1;
-import org.ts.teamstack.course.model.dto.Course;
-import org.ts.teamstack.user.model.dto.Users;
-import javax.servlet.http.HttpSession;
-import java.util.List;
-import java.util.Map;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -56,5 +50,35 @@ public class ClassPageController1 {
     @RequestMapping("/week")
     public String week() {
         return "classes/week";
+    }
+
+    @RequestMapping("/assign")
+    public String assign() {
+        return "classes/assign";
+    }
+
+    @RequestMapping("/file")
+    public String file() {
+        return "classes/file";
+    }
+
+//    @RequestMapping("/chatting")
+//    public String chatting() {
+//        return "classes/chatting";
+//    }
+
+    @RequestMapping("/atttend")
+    public String attend() {
+        return "classes/attend";
+    }
+
+    @RequestMapping("/score")
+    public String score() {
+        return "classes/score";
+    }
+
+    @RequestMapping("/calendar")
+    public String calendar() {
+        return "classes/calendar";
     }
 }
